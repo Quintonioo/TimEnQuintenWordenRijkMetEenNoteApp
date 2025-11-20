5eb1d52aedf08c65eead06aa8cd8199ae603eeb2
TimSmits321 <t.smits.0205@gmail.com>
2025-11-20 13:53:36 +0100

    Added note service, that handles creating and updating notes

--- noteApp/src/noteApp/SecureNote.java
+++ noteApp/src/noteApp/SecureNote.java
@@ -3,8 +3,13 @@ package noteApp;
 import java.security.NoSuchAlgorithmException;
 
 public class SecureNote extends Note {
-	
+
 	private String passwordHash;
+
+	public SecureNote(String name, String text, String password) throws NoSuchAlgorithmException {
+		super(name, text);
+		setPasswordHash(password);
+	}
 	
 	public void setPasswordHash(String password) throws NoSuchAlgorithmException {
 		this.passwordHash = Crypto.encryptPassword(password);
